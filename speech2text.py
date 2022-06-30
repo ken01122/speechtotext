23c1bfbe3c77f3cf5bcbaf0cdd18ef145a625fb2
ken01122 <ken99877@gmail.com>
2022-06-30 13:35:10 +0800

    textfile auto build and adjust

--- speech2text.py
+++ speech2text.py
@@ -3,8 +3,8 @@ import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence, detect_nonsilent
 
-path = "C:\\Users\\Ken\\Desktop\\project\\speech2text\\01.wav"
-write = 'C:\\Users\\Ken\\Desktop\\project\\speech2text\\output.txt'
+path = "C:\\Users\\Ken\\Desktop\\file\\123wav\\01.wav"
+write = 'C:\\Users\\Ken\\Desktop\\file\\speech2text\\output.txt'
 f = open(write, 'w')
 
 
@@ -20,7 +20,7 @@ audio_segment = AudioSegment.from_wav(path)
 # normalize audio_segment to -20dBFS
 normalized_sound = match_target_amplitude(audio_segment, -20.0)
 print("音訊長度={} seconds".format(len(normalized_sound) / 1000))
-f.write("音訊長度={} seconds".format(len(normalized_sound) / 1000) + "\n")
+
 # Print detected non-silent chunks, which in our case would be spoken words.
 nonsilent_data = detect_nonsilent(normalized_sound, min_silence_len=500, silence_thresh=-20, seek_step=1)
 
@@ -43,11 +43,11 @@ def get_large_audio_transcription(path):
     # split audio sound where silence is 700 miliseconds or more and get chunks
     chunks = split_on_silence(sound,
                               # experiment with this value for your target audio file
-                              min_silence_len=200,
+                              min_silence_len=500,
                               # adjust this per requirement
-                              silence_thresh=sound.dBFS - 10,
+                              silence_thresh=sound.dBFS - 20,
                               # keep the silence for 1 second, adjustable as well
-                              keep_silence=200,
+                              keep_silence=500,
                               )
     folder_name = "audio-chunks"
     sec = 0
@@ -70,13 +70,12 @@ def get_large_audio_transcription(path):
                 text = "can not recognize"
             else:
                 text = f"{text.capitalize()}. "
-            print([nonsilent_data[sec][0] / 1000, nonsilent_data[sec][1] / 1000], end=" ")
-            f.write(str([nonsilent_data[sec][0] / 1000, nonsilent_data[sec][1] / 1000]))
+            print([nonsilent_data[sec][0]/1000, nonsilent_data[sec][1]/1000], end=" ")
+            f.write(str([nonsilent_data[sec]]))
             print(text)
             f.write(text + "\n")
             f.write("\n")
             sec += 1
-    f.close
-    # return the text for all chunks detected
-
-get_large_audio_transcription(path)
+        f.close
+        # return the text for all chunks detected
+    get_large_audio_transcription(path)
